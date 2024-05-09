6adc8ec70cbc288fae75e2f3c15500fa1cc9e9fc
Andy Ron <randy.njfu@gmail.com>
2024-05-09 16:36:02 +0800

    Laravel Passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;  // TODO
+
+//use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
