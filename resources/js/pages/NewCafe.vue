3f9214febbeea113c306e34ee41a5ee6f1edf02a
Andy Ron <randy.njfu@gmail.com>
2024-06-15 00:53:57 +0800

    动态表单一次提交多个咖啡店位置信息

--- resources/js/pages/NewCafe.vue
+++ resources/js/pages/NewCafe.vue
@@ -7,40 +7,78 @@
                         <label>名称
                             <input type="text" placeholder="咖啡店名" v-model="name">
                         </label>
-                        <!-- 验证失败通知 -->
                         <span class="validation" v-show="!validations.name.is_valid">{{ validations.name.text }}</span>
                     </div>
                     <div class="large-12 medium-12 small-12 cell">
-                        <label>地址
-                            <input type="text" placeholder="地址" v-model="address">
+                        <label>网址
+                            <input type="text" placeholder="网址" v-model="website">
                         </label>
-                        <span class="validation" v-show="!validations.address.is_valid">{{ validations.address.text }}</span>
+                        <span class="validation" v-show="!validations.website.is_valid">{{ validations.website.text }}</span>
                     </div>
                     <div class="large-12 medium-12 small-12 cell">
+                        <label>简介
+                            <input type="text" placeholder="简介" v-model="description">
+                        </label>
+                    </div>
+                </div>
+                <div class="grid-x grid-padding-x" v-for="(location, key) in locations">
+                    <div class="large-12 medium-12 small-12 cell">
+                        <h3>位置</h3>
+                    </div>
+                    <div class="large-6 medium-6 small-12 cell">
+                        <label>位置名称
+                            <input type="text" placeholder="位置名称" v-model="locations[key].name">
+                        </label>
+                    </div>
+                    <div class="large-6 medium-6 small-12 cell">
+                        <label>详细地址
+                            <input type="text" placeholder="详细地址" v-model="locations[key].address">
+                        </label>
+                        <span class="validation" v-show="!validations.locations[key].address.is_valid">{{ validations.locations[key].address.text }}</span>
+                    </div>
+                    <div class="large-6 medium-6 small-12 cell">
                         <label>城市
-                            <input type="text" placeholder="城市" v-model="city">
+                            <input type="text" placeholder="城市" v-model="locations[key].city">
                         </label>
-                        <span class="validation" v-show="!validations.city.is_valid">{{ validations.city.text }}</span>
+                        <span class="validation" v-show="!validations.locations[key].city.is_valid">{{ validations.locations[key].city.text }}</span>
                     </div>
-                    <div class="large-12 medium-12 small-12 cell">
+                    <div class="large-6 medium-6 small-12 cell">
                         <label>省份
-                            <input type="text" placeholder="省份" v-model="state">
+                            <input type="text" placeholder="省份" v-model="locations[key].state">
                         </label>
-                        <span class="validation" v-show="!validations.state.is_valid">{{ validations.state.text }}</span>
+                        <span class="validation" v-show="!validations.locations[key].state.is_valid">{{ validations.locations[key].state.text }}</span>
                     </div>
-                    <div class="large-12 medium-12 small-12 cell">
+                    <div class="large-6 medium-6 small-12 cell">
                         <label>邮编
-                            <input type="text" placeholder="邮编" v-model="zip">
+                            <input type="text" placeholder="邮编" v-model="locations[key].zip">
                         </label>
-                        <span class="validation" v-show="!validations.zip.is_valid">{{ validations.zip.text }}</span>
+                        <span class="validation" v-show="!validations.locations[key].zip.is_valid">{{ validations.locations[key].zip.text }}</span>
                     </div>
                     <div class="large-12 medium-12 small-12 cell">
-                        <a class="button" v-on:click="submitNewCafe()">提交</a>
+                        <label>支持的冲泡方法</label>
+                        <span class="brew-method" v-for="brewMethod in brewMethods">
+                        <input v-bind:id="'brew-method-'+brewMethod.id+'-'+key" type="checkbox"
+                               v-bind:value="brewMethod.id"
+                               v-model="locations[key].methodsAvailable">
+                        <label v-bind:for="'brew-method-'+brewMethod.id+'-'+key">{{ brewMethod.method }}</label>
+                    </span>
+                    </div>
+                    <div class="large-12 medium-12 small-12 cell">
+                        <a class="button" v-on:click="removeLocation(key)">移除位置</a>
+                    </div>
+                </div>
+                <div class="grid-x grid-padding-x">
+                    <div class="large-12 medium-12 small-12 cell">
+                        <a class="button" v-on:click="addLocation()">新增位置</a>
+                    </div>
+                    <div class="large-12 medium-12 small-12 cell">
+                        <a class="button" v-on:click="submitNewCafe()">提交表单</a>
                     </div>
                 </div>
             </div>
         </form>
     </div>
+
 </template>
 
 <script>
@@ -48,43 +86,39 @@ export default {
     data() {
         return {
             name: '',
-            address: '',
-            city: '',
-            state: '',
-            zip: '',
+            locations: [],
+            website: '',
+            description: '',
+            roaster: false,
             validations: {
                 name: {
                     is_valid: true,
                     text: ''
                 },
-                address: {
+                locations: [],
+                oneLocation: {  // 确保咖啡店至少包含一个位置信息
                     is_valid: true,
                     text: ''
                 },
-                city: {
-                    is_valid: true,
-                    text: ''
-                },
-                state: {
-                    is_valid: true,
-                    text: ''
-                },
-                zip: {
+                website: {
                     is_valid: true,
                     text: ''
                 }
             }
         }
     },
+    created() {
+      this.addLocation();
+    },
     methods: {
         submitNewCafe: function () {
             if (this.validateNewCafe()) {
                 this.$store.dispatch('addCafe', {
                     name: this.name,
-                    address: this.address,
-                    city: this.city,
-                    state: this.state,
-                    zip: this.zip
+                    locations: this.locations,
+                    website: this.website,
+                    description: this.description,
+                    roaster: this.roaster
                 });
             }
         },
@@ -141,7 +175,85 @@ export default {
                 this.validations.zip.text = '';
             }
 
+            // 确保网址是有效的URL
+            if (this.website.trim() !== '' && !this.website.match(/^((https?):\/\/)?([w|W]{3}\.)?[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/)) {
+                validNewCafeForm = false;
+                this.validations.website.is_valid = false;
+                this.validations.website.text = '请输入有效的咖啡店网址';
+            } else {
+                this.validations.website.is_valid = true;
+                this.validations.website.text = '';
+            }
+
             return validNewCafeForm;
+        },
+        addLocation: function () {  // 用于新增一个位置区块到表单中，并在组件创建后进行调用
+            this.locations.push({name: '', address: '', city: '', state: '', zip: '', methodsAvailable: []});
+            this.validations.locations.push({
+               address: {
+                   is_valid: true,
+                   text: ''
+               },
+                city: {
+                    is_valid: true,
+                    text: ''
+                },
+                state: {
+                    is_valid: true,
+                    text: ''
+                },
+                zip: {
+                    is_valid: true,
+                    text: ''
+                }
+            });
+        },
+        removeLocation(key) {
+            this.locations.splice(key, 1);
+            this.validations.locations.splice(key, 1);
+        },
+        clearForm() { // 重置表单数据
+            this.name = '';
+            this.locations = [];
+            this.website = '';
+            this.description = '';
+            this.roaster = false;
+            this.validations = {
+                name: {
+                    is_valid: true,
+                    text: ''
+                },
+                locations: [],
+                oneLocation: {
+                    is_valid: true,
+                    text: ''
+                },
+                website: {
+                    is_valid: true,
+                    text: ''
+                }
+            };
+            // 清理完表单数据信息后，添加一个新的位置信息到表单
+            this.addLocation();
+        }
+    },
+    computed: {
+        brewMethods() {
+            return this.$store.getters.getBrewMethods();
+        },
+        addCafeStatus() {
+            return this.$store.getters.getCafeAddStatus;
+        }
+    },
+    watch: {  // TODO
+        'addCafeStatus': function () {
+            if (this.addCafeStatus === 2) {  // 添加成功
+                this.clearForm();
+                $("#cafe-added-successfully").show().delay(5000).fadeOut();
+            }
+            if (this.addCafeStatus === 3) {  // 添加失败
+                $("#cafe-added-unsuccessfully").show().delay(5000).fadeOut();
+            }
         }
     }
 }
